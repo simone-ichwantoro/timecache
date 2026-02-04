8df24f56c676cfa40ffae57663f4a61a61ce8e07
simone-ichwantoro <simonesabrawi@gmail.com>
2026-02-04 12:33:40 -0500

    yes

--- resources/script.js
+++ resources/script.js
@@ -1,22 +1,17 @@
 // Accordion functionality
-document.addEventListener("DOMContentLoaded", function() {
-  var acc = document.getElementsByClassName("accordion");
-  
-  for (var i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-      this.classList.toggle("active");
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-      /* Toggle between hiding and showing the active panel */
-      var panel = this.nextElementSibling;
-      if (panel && panel.classList.contains("panel")) {
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      }
-    });
-  }
-});
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  })}
+}
+</script>
